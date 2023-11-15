0ac2594a37f3d158f8b47dc3f8d8d0e1b57f9e8d
Zaid0412 <zaidahmed0412@gmail.com>
2023-11-15 15:23:37 +0300

    make func to get num1,num2 and operator and make calculator work for add,subtract,multiply and divide

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let num2;
 let operator;
 
 // MATH OPERATION FUNCTIONS
-const add = (a, b) => a + b;
+const add = (a, b) => +a + +b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
@@ -14,15 +14,13 @@ const percent = (a) => a / 100;
 
 // FUNCTION THAT RUNS OPERATIONS
 function operate(operator, a, b) {
-  if (operator === "+") add(a, b);
-  if (operator === "–") subtract(a, b);
-  if (operator === "×") multiply(a, b);
-  if (operator === "÷") divide(a, b);
-  if (operator === "xⁿ") power(a, b);
-  if (operator === "%") percent(a);
-  else {
-    console.log("operator not found");
-  }
+  if (!(operator || a || b)) return;
+  if (operator === "+") return add(a, b);
+  if (operator === "–") return subtract(a, b);
+  if (operator === "×") return multiply(a, b);
+  if (operator === "÷") return divide(a, b);
+  if (operator === "xⁿ") return power(a, b);
+  if (operator === "%") return percent(a);
 }
 
 // GET VALUE OF DISPLAY SCREEN
@@ -36,8 +34,10 @@ calculator.addEventListener("click", function (e) {
   const target = e.target.textContent;
   //checking for number values and decimal
   if (!isNaN(target) || target == ".") {
-    if (isNaN(displayScreen.textContent) && displayScreen.textContent != ".")
-      displayScreen.textContent = " ";
+    if (isNaN(displayScreen.textContent) && displayScreen.textContent != ".") {
+      display += ",";
+      displayScreen.textContent = " "; // clearing the screen only if the previous value was an operator
+    }
     displayScreen.textContent += target;
     display += target;
   }
@@ -47,5 +47,36 @@ calculator.addEventListener("click", function (e) {
     display += target;
   }
   if (e.target.classList.contains("key")) display += target;
-  console.log(display);
+  // console.log(display);
 });
+
+// GET FIRST NUMBER (from display)
+const getNum1 = function () {
+  num1 = display.replace(/[^0-9.].*/, "");
+  console.log(num1);
+  return num1;
+};
+
+// GET SECOND NUMBER (from display)
+const getNum2 = function () {
+  const num = display.slice(display.indexOf(",") + 1);
+  num2 = num.slice(0);
+  return num2;
+};
+
+// GET OPERATOR (from display)
+const getOperator = function () {
+  operator = display.slice(display.indexOf(",") - 1, display.indexOf(","));
+  return operator;
+};
+
+// RUN OPERATION
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", () => {
+  let res = operate(getOperator(), getNum1(), getNum2());
+  displayScreen.textContent = res;
+});
+
+// CLEAR DISPLAYS SCREEN
+// const clear = document.querySelector(".clear");
+// clear.addEventListener("click", function () {});
